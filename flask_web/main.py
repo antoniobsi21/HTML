8d62e802497bac9b2a1d5acfca0305441470f331
Antônio <antonio.fernades.bsi@gmail.com>
2019-04-22 16:13:33 -0300

    Ainda integrando com flask

--- flask_web/main.py
+++ flask_web/main.py
@@ -1,25 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 regioes = ['BR', 'NA', 'EUNE', 'EUW', 'JP', 'KR', 'LAS', 'LAN', 'OCE', 'TR', 'RU', 'PBE']
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	contexto = {'titulo': 'Página Inicial'}
+	return render_template('index.html', contexto=contexto)
 
 @app.route('/cadastro')
 def cadastro():
-	contexto = {'regioes': regioes}
+	contexto = {'regioes': regioes, 'titulo': 'Cadastro'}
 	return render_template('cadastro.html', contexto=contexto)
 
 @app.route('/contato')
 def contato():
-	return render_template('contato.html')
+	contexto = {'titulo': 'Contato'}
+	return render_template('contato.html', contexto=contexto)
 
 @app.route('/login')
 def login():
-	return render_template('login.html')
+	contexto = {'titulo': 'Login'}
+	return render_template('login.html', contexto=contexto)
 
-@app.route('/invocador/<nome>')
-def invocador(nome):
-	return render_template('invocador.html', nome=nome)
+@app.route('/invocador', methods=['GET','POST'])
+def invocador():
+	if request.form.get("invocador"):
+		nome = request.form["invocador"]
+	else:
+		nome = ""
+		print("@@@@")
+		print(request.form)
+		# return redirect(url_for("index"))
+	contexto = {'nome': nome, 'titulo': 'Invocador - {}'.format(nome)}
+	return render_template('invocador.html', contexto=contexto)
