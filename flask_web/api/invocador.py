98b99c942d5a34f251585c5b776cbb3a3a708225
Antônio <antonio.fernades.bsi@gmail.com>
2019-04-24 16:32:12 -0300

    A little bit more

--- flask_web/api/invocador.py
+++ flask_web/api/invocador.py
@@ -1,28 +1,22 @@
 import requests
 
-myKey = 'não sei, tem que'
 myKey= 'RGAPI-6c6426ef-273b-4d58-a486-189e9377fabf'
 class Invocador:
 	def __init__(self, nickname, regiao):
 		self.nickname = nickname
-		self.region = regiao.lower() + str(1) if regiao not in ['kr', 'ru'] else regiao
+		regiao = regiao.lower()
+		self.region = regiao + str(1) if regiao not in ['kr', 'ru'] else regiao
 		self.req = requests.get('https://{}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}?api_key={}'. \
 		format(self.region, self.nickname, myKey))
-		# if req.status_code == 404:
-		# 	print("Invocador não encontrado.")
-		# elif req.status_code == 403:
-		# 	print("Chave inválida, por favor verifique a válidade de sua chave.")
-		# elif req.status_code == 200:
 		self.dados = self.req.json()
 
-	def getSoloQueuePosition(self):
+	def getSoloQueueInformation(self):
 		elo = requests.get('https://br1.api.riotgames.com/lol/league/v4/positions/by-summoner/{}?api_key=RGAPI-a351e1cf-d618-489a-9545-df3d85cf521d'. \
 			format(self.dados['id']))
 		self.elo = elo.json()
 		for x in self.elo:
 			if x['queueType'].upper() == "RANKED_SOLO_5X5":
-				print("\n{}: {} {} ({} PdL).\n".format(self.dados['name'], x['tier'], x['rank'], x['leaguePoints']))
-				break
+				return x
 		return -1
 
 	def checarPartidaAtiva(self):
@@ -33,19 +27,12 @@ class Invocador:
 			print('(Em partida: encryptionKey: \'{}\', matchId: \'{}\', platformId: \'{}\'.)\n'. \
 			format(partidaAtiva['observers']['encryptionKey'], partidaAtiva['gameId'], partidaAtiva['platformId']))
 
-	def printAllQueuePositions(self):
+	def getAllQueueInformation(self):
 		elo = requests.get('https://{}.api.riotgames.com/lol/league/v4/positions/by-summoner/{}?api_key={}'. \
 		format(self.region, self.dados['id'], myKey))
+		positions = {}
 		self.elo = elo.json()
-		print("\n{:^20}\n".format(self.dados['name']))
-		self.checarPartidaAtiva()
-		for x in self.elo:
-			if x['queueType'].upper() == "RANKED_SOLO_5X5":
-				print("Solo:")
-			if x['queueType'].upper() == "RANKED_FLEX_SR":
-				print("Flex:")
-			if x['queueType'].upper() == "RANKED_FLEX_3X3":
-				print("3x3:")
-			print("{}: {} {} ({} PdL).\n".format(x['leagueName'], x['tier'], x['rank'], x['leaguePoints']))
-		return -1
-
+		ordenedQueues = {}
+		for x in sorted(self.elo):
+			ordenedQueues[x] = self.elo[x]
+		return ordenedQueues
--- flask_web/main.py
+++ flask_web/main.py
@@ -9,28 +9,30 @@ from api.invocador import Invocador
 # myKey = 'não sei, tem que ver'
 # Se importar não sera necessário refazer ou criar código aqui, somente criar possíveis mais códigos na classe
 # Invocador
-
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'senha dificil de quebrar'
 regioes = ['BR', 'NA', 'EUNE', 'EUW', 'JP', 'KR', 'LAS', 'LAN', 'OCE', 'TR', 'RU', 'PBE']
+contexto = {"regioes": regioes, "selecionado": 'BR'}
+print("-----")
 
 @app.route('/')
 def index():
-	contexto = {'titulo': 'Página Inicial', 'regioes': regioes}
+	contexto['titulo'] = 'Página Inicial'
 	return render_template('index.html', contexto=contexto)
 
 @app.route('/cadastro')
 def cadastro():
-	contexto = {'regioes': regioes, 'titulo': 'Cadastro'}
+	contexto['titulo'] = 'Cadastro'
 	return render_template('cadastro.html', contexto=contexto)
 
 @app.route('/contato')
 def contato():
-	contexto = {'titulo': 'Contato'}
+	contexto['titulo'] = 'Contato'
 	return render_template('contato.html', contexto=contexto)
 
 @app.route('/login')
 def login():
-	contexto = {'titulo': 'Login'}
+	contexto['titulo'] = 'Login'
 	return render_template('login.html', contexto=contexto)
 
 @app.route('/invocador', methods=['GET','POST'])
@@ -44,20 +46,34 @@ def invocador():
 		dados = invocador.dados
 		
 		statusReq = invocador.req.status_code
+
+		contexto['icone'] = str(dados['profileIconId'])
+		contexto['selecionado'] = regiao
+
+		print(contexto['icone'])
+		print(invocador.req)
+		print('\n\n{}'.format(dados))
+
 		if statusReq == 200:
 			nome = dados['name']
 			nivel = dados['summonerLevel']
-			contexto = {'titulo': 'invocador - {}'.format(nome), 'invocador': nome, 'nivel': nivel}
+			contexto['titulo'] = 'Invocador - {}'.format(nome)
+			contexto['dados'] = dados
 			return render_template('invocador.html', contexto=contexto)
-		# elif statusReq == 403:
-		# 	error = 'Chave provavelmente inválida, verifique a validade da mesma'
-		# elif statusReq == 404:
-		# 	error = 'Invocador não existe na região informada.'
-		# redirect(url_for('index', error=error))
-
+		elif statusReq == 403:
+			error = 'Chave provavelmente inválida, verifique a validade da mesma'
+		elif statusReq == 404:
+			error = 'Invocador não encontrado na região informada.'
+		elif statusReq == 400:
+			error = 'Possíveis caracteres inválidos. (Bad request 400)'
+		redirect(url_for('index', error=error))
+		flash(error)
 		return redirect(url_for('index'))
-		# NÃO SEI
-	except:
+	except Exception as e:
+		print("\n\n")
+		print(e)
+		print("\n\n")
+		flash("Erro desconhecido!")
 		return redirect(url_for('index'))
 
 
